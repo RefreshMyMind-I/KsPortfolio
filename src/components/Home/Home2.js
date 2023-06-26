9dd882bc37394175184ea12a8c11197d1280686d
RefreshMyMind-I <refreshmymind0@gmail.com>
2023-06-26 16:28:26 +0530

    removed unused component from home/home2.js

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,10 +1,8 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import Tilt from "react-parallax-tilt";
 import {
   AiFillGithub,
   AiOutlineTwitter,
-  AiFillInstagram,
 } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
 
