7eb8d726381147733e51445249a9d19d3648bc5b
RefreshMyMind-I <refreshmymind0@gmail.com>
2023-06-26 16:27:37 +0530

    removed unused component from tookstack

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -2,10 +2,6 @@ import React from "react";
 import { Col, Row } from "react-bootstrap";
 import {
   SiVisualstudiocode,
-  SiPostman,
-  SiSlack,
-  SiVercel,
-  SiMacos,
   SiLinux,
   SiGithub,
   SiGimp,
