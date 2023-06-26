fc3411d08f05658698346c3942134c4df34d33a9
RefreshMyMind-I <refreshmymind0@gmail.com>
2023-06-26 16:28:00 +0530

    removed unused component from footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,20 +1,10 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
-import {
-  AiFillGithub,
-  AiOutlineTwitter,
-  AiFillInstagram,
-} from "react-icons/ai";
-import { FaLinkedinIn } from "react-icons/fa";
+import { Container, Row, } from "react-bootstrap";
 
 function Footer() {
-  let date = new Date();
-  let year = date.getFullYear();
   return (
     <Container fluid className="footer">
       <Row>
-
-        
       </Row>
     </Container>
   );
